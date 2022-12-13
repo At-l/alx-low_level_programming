a7b92b8c7aa66133f33c5b029767da5d01c17ca9
At-l <shonkieraphiri@gmail.com>
2022-12-13 12:38:22 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int _print_sign(int n);
+int _abs(int a);
+int print_last_digit(int num);
+void jack_bauer(void);
+void times_table(void);
+int add(int num1, int num2);
+void print_to_9B(int n);
+
+#endif
