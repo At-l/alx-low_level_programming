71499a249ead51b65bf830e8016cf87f587334af
At-l <shonkieraphiri@gmail.com>
2022-12-21 13:25:28 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts -main entry.
+ * void: nothing
+ * @str: char pointer
+ * Description:  function that prints a string, followed by a new line
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != '\0'; c++)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
+
+}
