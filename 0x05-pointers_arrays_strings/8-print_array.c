e95fa4c97e4b5d0e5ff511e4faeacf6fad9beb09
At-l <shonkieraphiri@gmail.com>
2022-12-21 13:58:19 -0800

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array -main entry.
+ * void: nothing
+ * @a: int pointer
+ * @n: integer number of elements of the array to be printed
+ * Description: function prints elements of array of integers
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int c;
+
+/*the array counts from position 0 for that reason I used n -1 */
+	for (c = 0; c < n - 1; c++)
+	{
+		printf("%d, ", a[c]);
+	}
+	if (c == n - 1)
+		printf("%d", a[c]);
+
+	printf("\n");
+}
